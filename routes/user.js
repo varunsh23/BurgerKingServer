3ed8068253497f19866788a4e579a50d59572d1a
Varun Sharma <varun.1822en1163@kiet.edu>
2023-04-19 21:09:14 +0530

    Initial Commit

---
+++ routes/user.js
@@ -0,0 +1,31 @@
+import express from "express";
+import passport from "passport";
+import {
+  logout,
+  myProfile,
+  getAdminStats,
+  getAdminUsers,
+} from "../controllers/user.js";
+import { authorizeAdmin, isAuthenticated } from "../middlewares/auth.js";
+
+const router = express.Router();
+router.get(
+  "/googlelogin",
+  passport.authenticate("google", {
+    scope: ["profile"],
+  })
+);
+router.get(
+  "/login",
+  passport.authenticate("google", {
+    successRedirect: process.env.FRONTEND_URL,
+  })
+);
+router.get("/me", isAuthenticated, myProfile);
+router.get("/logout", logout);
+
+// Admin Routes
+router.get("/admin/users", isAuthenticated, authorizeAdmin, getAdminUsers);
+
+router.get("/admin/stats", isAuthenticated, authorizeAdmin, getAdminStats);
+export default router;
