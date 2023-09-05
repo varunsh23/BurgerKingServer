4af0c20f9627d3671bacaa80d3877a359dd8539b
Varun Sharma <waynevsstark@gmail.com>
2023-09-05 15:35:47 +0530

    Third Commit

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use(
 app.use(
   cors({
     credentials: true,
+    allowedHeaders:['Content-Type', 'Authorization'],
     origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
